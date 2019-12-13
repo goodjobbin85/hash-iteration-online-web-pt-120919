e97a6d039ea4582c9216104674c8a3b89ad690f0
thomas dubiel <td1729@hotmail.com>
2019-12-13 21:00:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,10 @@ def happy_birthday(birthday_kids)
   end
 end
 
+def age_appropriate_birthday(birthday_kids) 
+  birthday_kids.each do |kid, age| 
+    if age < 
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  end
+end
 
